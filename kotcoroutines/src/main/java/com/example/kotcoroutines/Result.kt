635b3db029c59f25aba8dd67ee52898bb05776b8
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-12-13 23:32:30 +0100

    initial commit

---
+++ kotcoroutines/src/main/java/com/example/kotcoroutines/Result.kt
@@ -0,0 +1,9 @@
+package com.example.kotcoroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//Repository class and making the network request
+sealed class Result<out R> {
+
+}
