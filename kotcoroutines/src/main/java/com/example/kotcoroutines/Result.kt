46d2fc209d53d17a3a6fb3dcdbaddae860d9d208
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2021-12-14 19:57:06 +0100

    implementing suspend functions

--- kotcoroutines/src/main/java/com/example/kotcoroutines/Result.kt
+++ kotcoroutines/src/main/java/com/example/kotcoroutines/Result.kt
@@ -1,9 +1,39 @@
 package com.example.kotcoroutines
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import kotlinx.coroutines.*
+import java.lang.Exception
 
-//Repository class and making the network request
+//Executing in a background thread
+// Repository class and making the network request
+
 sealed class Result<out R> {
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+}
+fun main() = runBlocking {
+    launch {
+        delay(1000L) //non-blocking for 1 second (default time unit is ms)
+        println("World!") // print after delay
+    }
+    println("Hello") //main coroutine continues while a previous one is delayed
+}
 
+fun main0() = runBlocking {
+    launch { doWorld() }
+    println("Hello")
+}
+suspend fun doWorld() {
+    delay(1000L)
+    println("World")
+}
+fun main1() = runBlocking {
+    doWorld0()
+}
+
+suspend fun doWorld0() = coroutineScope {  // this: CoroutineScope
+    launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello")
 }
